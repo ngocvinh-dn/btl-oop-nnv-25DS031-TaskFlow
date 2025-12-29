366ab4cb3962ef6b4fc656932092d1161768c474
ngocvinh-dn <ngocvinhdn1604@gmail.com>
2025-12-29 10:09:24 +0700

    structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module application {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.j;
+    requires javafx.graphics;
+    requires java.prefs;
+
+
+}
